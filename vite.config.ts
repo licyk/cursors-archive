ce9babafbe4af1d60af58c4e6b4a43d2d839d1bf
licyk <licyk233@163.com>
2026-05-21 17:37:15 +0000

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,7 @@ import { cursorCatalogPlugin } from './build/cursorCatalogPlugin'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './',
+  cacheDir: 'node_modules/.vite',
   plugins: [cursorCatalogPlugin(), vue()],
 })
